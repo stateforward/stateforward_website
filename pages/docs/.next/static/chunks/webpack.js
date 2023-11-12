110bc6d1b5ec44fc86e0ced5ab53727370663513
Jeffrey Jewett <jeffrey.jewett@gmail.com>
2023-11-12 13:37:59 -0500

    Docs Update

--- pages/docs/.next/static/chunks/webpack.js
+++ pages/docs/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aa2273acdd97a5a7"; }
+/******/ 		__webpack_require__.h = function() { return "ad33c64996491256"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/docs/theme.config.tsx
+++ pages/docs/theme.config.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Image src="https://stateforward.org/_next/static/media/stateforward.2772b883.svg" alt="Stateforward Logo" width={220} height={30} />,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
